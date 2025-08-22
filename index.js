4812e51f6425671e7304b7abdd6b0eccbf4369bd
Darwin Ubeda <darwin.ubeda2025@gmail.com>
2025-08-22 14:58:27 -0600

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const boton_entrar = document.getElementById('boton_entrar');
-
-boton_entrar.addEventListener('click', (e) => {
-  // redirige inmediatamente
-  window.location.href = './pokedex.html';
-
-});
-
